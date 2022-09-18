a2f3a5abc2da474b014ea8ad95ae70e7ee72e8b4
marvelman3284 <jedimaster3284@gmail.com>
2022-09-17 20:17:13 -0400

    serpinski hexagon

--- src/shah/jeevan/Main.java
+++ src/shah/jeevan/Main.java
@@ -2,33 +2,36 @@ package shah.jeevan;
 
 public class Main {
 
+    public static double[][] points = new double[7][2];
     public static Turtle t = new Turtle();
 
     public static void main(String[] args) {
-        t.speed(10);
-        t.setPosition(0, 0);
-        tiltTriangle(100, 120, 0, 30);
-        wait(500);
-        t.clear();
-        serpinski(-100, -100, 0, 100, 100, -100, 5);
-        wait(500);
-        t.clear();
-        recursiveSquare(300, 0, 0, 3);
-        wait(500);
-        t.clear();
-        t.speed(15);
-        t.setDirection(90);
-        t.up();
-        t.setPosition(0, -200);
-        t.down();
-        tree(100);
-        wait(500);
-        t.clear();
-        t.speed(1);
-        recursiveH(0, 0, 150, 4);
-        wait(500);
-        t.clear();
-        recursiveHexagon(100, 0, 0, 5);
+//        t.speed(10);
+//        t.setPosition(0, 0);
+//        tiltTriangle(100, 120, 0, 30);
+//        wait(500);
+//        t.clear();
+//        serpinski(-100, -100, 0, 100, 100, -100, 5);
+//        wait(500);
+//        t.clear();
+//        recursiveSquare(300, 0, 0, 3);
+//        wait(500);
+//        t.clear();
+//        t.speed(15);
+//        t.setDirection(90);
+//        t.up();
+//        t.setPosition(0, -200);
+//        t.down();
+//        tree(100);
+//        wait(500);
+//        t.clear();
+//        t.speed(1);
+//        recursiveH(0, 0, 150, 4);
+//        wait(500);
+//        t.clear();
+//        recursiveHexagon(100, 0, 0, 5);
+        t.speed(0);
+        hexa(1000000);
     }
 
     private static void wait(int time) {
@@ -183,5 +186,41 @@ public class Main {
             t.right(20);
             t.backward(length);
         }
+    }
+
+    public static void hexa(int iters) {
+        if (iters < 1) {
+            return;
+        }
+        t.up();
+        t.setPosition(100, 150);
+        for (int i=0; i<6; i++) {
+            t.dot();
+            points[i][0] = t.getX();
+            points[i][1] = t.getY();
+            t.right(60);
+            t.forward(200);
+        }
+        points[6][0] = 0;
+        points[6][1] = 0;
+        for (int i=0; i<iters; i++) {
+            int rand = rand(0, 5);
+            t.setPosition(points[6][0], points[6][1]);
+            double dist = distance(points[6][0], points[6][1], points[rand][0], points[rand][1]);
+            t.face(points[rand][0], points[rand][1]);
+            t.forward(dist);
+            t.dot();
+            points[6][0] = t.getX();
+            points[6][1] = t.getY();
+        }
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return (2.0d/3.0d)*(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2)));
+    }
+
+    public static int rand(int min, int max) {
+        int range = max-min+1;
+        return (int)(Math.floor((Math.random() * range) + min));
     }
 }
